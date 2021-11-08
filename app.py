9c2fe1ae25ce1e28db93922327df0d26f94b9f21
archit009 <archithinge9@gmail.com>
2021-11-08 18:18:22 +0530

    triggering circleci

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
 
-    # TO DO:  Log the output prediction value
+    # TO DO:  Logging out the output value
     LOG.info(f"Prediction returned log: \n{prediction}")
     return jsonify({'prediction': prediction})
 
